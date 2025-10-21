80654c4060e07b29d2cc38073eada1d717532940
Murodillaxon <murka.surka.a4@gmail.com>
2025-10-21 22:38:36 +0500

    login up date

--- src/contexts/SocketContext.jsx
+++ src/contexts/SocketContext.jsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { message } from 'antd';
 
 // Укажите ваш адрес сервера
-const SOCKET_URL =  'http://localhost:3001';
+const SOCKET_URL =  'https://shashkaback-production.up.railway.app/';
 
 const SocketContext = createContext();
 
